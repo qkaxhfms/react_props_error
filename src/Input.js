5bb88f88d25db70bec1744911228bc845f5bf14e
unknown <kwakms821@daum.net>
2020-04-24 00:40:36 +0900

    input.js

--- src/Input.js
+++ src/Input.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Input() {
+  const [input, setInput] = useState("");
+
+  const change = (e) => {
+    // console.log(e.target.value);
+    setInput(e.target.value);
+  };
+  const reset = () => {
+    setInput("");
+  };
+
   return (
     <>
       <div>
-        <input type="text" />
-        <button>초기화</button>
+        <input type="text" onChange={change} value={input} />
+        <button onClick={reset}>초기화</button>
         <p>
           <b>값:</b>
-          <span>...</span>
+          <span>{input}</span>
         </p>
       </div>
     </>
