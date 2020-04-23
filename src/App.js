4598596c7184964fb6f2d046a9a04fd8591c711f
unknown <kwakms821@daum.net>
2020-04-23 23:42:52 +0900

    props

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Hi from './Hi';
 
 function App() {
   return (
-    <Hi name="민상"/>
+
+    <Hi name="민상" firend="친구"/>
+
   );
 }
 
--- src/Hi.js
+++ src/Hi.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 function Hi(props){
+    const {name, friend} = props;
     return(
-    <h1>Hi!!{props.name}</h1>
+        <>
+        <h1>Hi!!{props.name}</h1>
+        <h2>내 {props.firend}</h2>
+        </>
     )
 }
 
