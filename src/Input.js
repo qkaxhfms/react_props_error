b4e8dbf58392556d93ac122060de8aee5805a762
unknown <kwakms821@daum.net>
2020-04-24 00:52:09 +0900

    input check

--- src/Input.js
+++ src/Input.js
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
 
 function Input() {
-  const [input, setInput] = useState("");
+  const [inputs, setInput] = useState({
+    phone: "",
+    email: "",
+  });
 
+  const { phone, email } = inputs;
   const change = (e) => {
-    // console.log(e.target.value);
-    setInput(e.target.value);
+    console.log(e.target.value);
+    console.log(e.target.name);
+
+    const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
   };
-  const reset = () => {
-    setInput("");
-  };
+  const reset = () => {};
 
   return (
     <>
       <div>
-        <input type="text" onChange={change} value={input} />
+        <input name="phone" placeholder="폰번" onChange={change} />
+        <input name="email" placeholder="이메일" onChange={change} />
         <button onClick={reset}>초기화</button>
-        <p>
-          <b>값:</b>
-          <span>{input}</span>
-        </p>
+        <ul>
+          <li>폰번호: {phone}</li>
+          <li>이메일: {email}</li>
+        </ul>
       </div>
     </>
   );
