ad33f3d23cbda1b3a7141975ae876866b57eb3f7
unknown <kwakms821@daum.net>
2020-04-23 23:38:10 +0900

    react day2

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hi from './Hi';
+
+function App() {
+  return (
+    <Hi name="민상"/>
+  );
+}
+
+export default App;
---
+++ src/Hi.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Hi(props){
+    return(
+    <h1>Hi!!{props.name}</h1>
+    )
+}
+
+export default Hi;
