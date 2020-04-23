c65e164981f7289a19dd51156740cf2dafc8b791
unknown <kwakms821@daum.net>
2020-04-23 23:53:12 +0900

    조건부 렌더링 사전작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hi from './Hi';
+import Condition from './Condition';
 
 function App() {
   return (
 
-    <Hi name="민상" friend="친구" one="1"/>
+    <>
+      <Hi name="민상" friend="친구" one="1"/>
+      <Condition />
+    </>
 
   );
 }
---
+++ src/Condition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Condition(){
+    return(
+        <>
+            <div>리액트 두번째 날</div>
+        </>
+    )
+}
+
+export default Condition;
